6fe50b86bd20ad1128d8771208e1451388016728
Mrinmoy Mondal <mrinmoymondalreal@gmail.com>
2024-04-28 14:43:12 +0530

    fix svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.argv.includes('dev');
 
@@ -10,6 +10,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		alias: {
+      "@/*": "./src/lib/*"
+    },
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH,
 		}
