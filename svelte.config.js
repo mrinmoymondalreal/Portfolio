2dbedcec24a6368ad2c295aef394ffe5c23b4d46
Mrinmoy Mondal <mrinmoymondalreal@gmail.com>
2024-04-28 15:11:07 +0530

    fix svelte config #1

--- svelte.config.js
+++ svelte.config.js
@@ -9,10 +9,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
 		alias: {
       "@/*": "./src/lib/*"
-    }
+    },
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	},
 	preprocess: [vitePreprocess()]
 };
