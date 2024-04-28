015df83a3840ba7249302bce4290d746382eb3c9
Mrinmoy Mondal <mrinmoymondalreal@gmail.com>
2024-04-28 15:18:27 +0530

    fix svelte config #2

--- d.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-const files = fs.readdirSync('./static');
-let list = [];
-files.forEach((file, i) => {
-  list.push(file);
-
-});
-console.log(list);
-
--- server.js
+++
@@ -1,7 +0,0 @@
-import http from 'http';
-
-http.createServer((req, res)=>{
-	if(req.url == '/'){
-		console.log(res);
-	}
-}).listen(3000);
