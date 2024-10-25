6b61755c38cda3adc1e6cbe6ef7c5a143330968a
Mrinmoy Mondal <mrinmoymondalreal@gmail.com>
2024-10-25 13:11:27 +0530

    minor bug;fix #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./app.css";
 import { AboutMeSection } from "./components/aboutme.section";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
